9818af7adf09bc77247ad0efb961ebb64fa51892
jackwill <wujackwill@outlook.com>
2024-01-22 15:32:24 +0800

    fix: delete ast_grep

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -69,7 +69,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { 'tsserver', 'rust_analyzer', 'ast_grep' },
+    ensure_installed = { 'tsserver', 'rust_analyzer' },
     handlers = {
         lsp_zero.default_setup,
     },
